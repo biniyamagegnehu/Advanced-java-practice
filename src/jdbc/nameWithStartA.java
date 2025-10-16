14b26d7977ef9f9e74321bac4b13c65eb952e118
biniyamagegnehu <biniyamagegnehu2@gmail.com>
2025-10-16 23:18:09 +0300

    get name start with D

---
+++ src/jdbc/nameWithStartA.java
@@ -0,0 +1,29 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class nameWithStartA {
+    public static void main(String[] args) throws SQLException,ClassNotFoundException {
+         Class.forName("com.mysql.cj.jdbc.Driver");
+         Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", "root", "");
+         System.out.println("Connected to the database successfully!");
+
+        String sql = ("SELECT FirstName , LastName FROM Employees WHERE FirstName LIKE 'D%'");
+        PreparedStatement ps = con.prepareStatement(sql);
+        
+        ResultSet rs  = ps.executeQuery();
+
+        if (rs.next()) {
+            String firstName = rs.getString("FirstName");
+            String lastName = rs.getString("LastName");
+            System.out.println("Full name  " + firstName + " " + lastName);
+        }
+        ps.close();
+        rs.close();
+        con.close();
+    }
+}
